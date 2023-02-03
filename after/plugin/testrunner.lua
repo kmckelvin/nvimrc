c5358687477cd342940f55c2a103a450410835d4
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-03 12:46:15 +0000

    Configure ava test runner

--- after/plugin/testrunner.lua
+++ after/plugin/testrunner.lua
@@ -5,3 +5,4 @@ vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
 vim.keymap.set("n", "<leader>tg", ":TestVisit<CR>")
 
 vim.cmd('let test#strategy="neovim"')
+vim.cmd("let test#javascript#ava#file_pattern = '\\v.*\\.ava\\.(t|j)s(x?)$'")
