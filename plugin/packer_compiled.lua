a7a1db331d8692a971563f802e3268026d702e26
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-04 22:29:54 +0000

    Add textobjects through treesitter from kickstart

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -187,6 +187,11 @@ _G.packer_plugins = {
     path = "/Users/kevin/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
+  ["nvim-treesitter-textobjects"] = {
+    loaded = true,
+    path = "/Users/kevin/.local/share/nvim/site/pack/packer/start/nvim-treesitter-textobjects",
+    url = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects"
+  },
   ["onedark.vim"] = {
     loaded = true,
     path = "/Users/kevin/.local/share/nvim/site/pack/packer/start/onedark.vim",
