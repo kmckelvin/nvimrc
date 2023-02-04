f4229666038d071ee37992330335618a73945cc0
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-04 11:55:17 +0000

    Fix line movement

--- lua/kmckelvin/keymap.lua
+++ lua/kmckelvin/keymap.lua
@@ -12,12 +12,10 @@ vim.keymap.set("n", "t[", ":tabprevious<CR>")
 vim.keymap.set("n", "<leader>fs", ":w<CR>")
 vim.keymap.set("n", "<C-s>", "<esc>:w<CR>")
 vim.keymap.set("i", "<C-s>", "<esc>:w<CR>l")
-
-
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- paste without changing buffer
 vim.keymap.set("x", "<leader>p", "\"_dP")
