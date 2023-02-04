4cbc21e7f4e561098eac8bf4312a2ecf96010937
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-04 23:25:25 +0000

    Use telescope for more lsp functions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,15 +23,17 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
   end
 
+  local telescope = require('telescope.builtin')
+
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gi', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('gy', vim.lsp.buf.type_definition, '[G]oto T[y]pe definition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+  nmap('gr', telescope.lsp_references, '[G]oto [R]eferences')
+  nmap('gi', telescope.lsp_implementations, '[G]oto [I]mplementation')
+  nmap('gy', telescope.lsp_type_definitions, '[G]oto T[y]pe definition')
+  nmap('<leader>ds', telescope.lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', telescope.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
 
   -- See `:help K` for why this keymap
   nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
