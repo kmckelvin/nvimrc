2737c83b2d546dd9da2519f63dd036e2b74191fb
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-05 23:01:18 +0000

    Colors for added lines in git diffs

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -3,3 +3,5 @@ vim.cmd.colorscheme("codedark")
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "lightblue" })
 vim.api.nvim_set_hl(0, "Normal", { bg = "#1a1a1a" })
+
+vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#136f13" }) -- fugitive git diffs
