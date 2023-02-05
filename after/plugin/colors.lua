2433b414922482bc3a016c553dfcb52e5334857f
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-05 22:08:00 +0000

    Remove commented code from colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,8 +1,5 @@
 vim.cmd.colorscheme("codedark")
 
--- vim.highlight.CursorLine = { guibg = "#222222" }
--- vim.highlight.LineNr = { guifg = "lightblue" }
-
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#222222" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "lightblue" })
 vim.api.nvim_set_hl(0, "Normal", { bg = "#1a1a1a" })
