2d36a0e93609292708c3acf3e08dbc09be6a7bb4
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-05 22:08:22 +0000

    Set tmux window name on bufenter

--- lua/kmckelvin/tmux.lua
+++ lua/kmckelvin/tmux.lua
@@ -1,6 +1,6 @@
 if vim.fn.exists("$TMUX") then
   vim.api.nvim_create_autocmd(
-    { "VimEnter", "VimResume", "BufReadPost", "FileReadPost", "BufNewFile" },
+    { "VimEnter", "VimResume", "BufReadPost", "FileReadPost", "BufEnter" },
     { command = 'call system("tmux rename-window vi:" . fnamemodify(getcwd(), ":t"))' }
   )
 
