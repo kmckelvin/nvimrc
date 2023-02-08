5a512d713ab15e0ec88548c16349628e0a71d158
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-08 21:55:57 +0000

    Set blamer color to grey

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,3 +5,5 @@ vim.api.nvim_set_hl(0, "LineNr", { fg = "lightblue" })
 vim.api.nvim_set_hl(0, "Normal", { bg = "#1a1a1a" })
 
 vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#136f13" }) -- fugitive git diffs
+
+vim.api.nvim_set_hl(0, "Blamer", { fg = "#666666" })
