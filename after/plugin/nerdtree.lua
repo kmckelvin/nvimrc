10ffd35428f3e7c85b0fa73c7348fb82db369a10
Kevin McKelvin <kmckelvin@gmail.com>
2023-02-27 23:39:48 +0000

    Delete nerdtree file

--- after/plugin/nerdtree.lua
+++
@@ -1,6 +0,0 @@
--- vim.keymap.set("n", "<leader>ft", ":NERDTreeToggle<CR>")
--- vim.keymap.set("n", "<leader>ff", ":NERDTreeFind<CR>")
-
--- vim.api.nvim_create_autocmd("BufEnter", {
---   command = 'if (&modifiable && g:NERDTree.IsOpen() && expand("%:p")[0] == "/" && expand("%")[0:9] != "NERD_tree_" && expand("%:t") != "COMMIT_EDITMSG") | NERDTreeFind | wincmd p | endif'
--- })
