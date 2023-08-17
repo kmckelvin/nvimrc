cf1f4aa7f1463edf88418b28a6ec0dec783d5498
Kevin McKelvin <kmckelvin@gmail.com>
2023-08-17 11:12:28 +0100

    keymap: Edit file under cursor

--- lua/kmckelvin/keymap.lua
+++ lua/kmckelvin/keymap.lua
@@ -41,3 +41,26 @@ vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]])
 vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]])
 vim.keymap.set('t', '<esc><esc>', [[<Cmd>ToggleTerm<CR>]])
 vim.keymap.set('t', '<leader><esc>', [[<C-\><C-n>]])
+
+local function edit_file_under_cursor()
+  vim.cmd('normal! viWo"py')
+  local grabbed_string = vim.fn.getreg('p')
+  vim.api.nvim_input('<ESC>')
+
+  local file, line_number = string.match(grabbed_string, "(.-):([0-9]+)")
+
+  if line_number == nil then
+    file = grabbed_string
+  end
+
+  if vim.loop.fs_stat(vim.fn.getcwd() .. '/' .. file) ~= nil then
+    vim.cmd('wincmd p')
+    vim.cmd('e ' .. file)
+
+    if line_number ~= nil then
+      vim.cmd('normal ' .. line_number .. 'gg')
+    end
+  end
+end
+
+vim.keymap.set('n', '<leader>gf', edit_file_under_cursor)
